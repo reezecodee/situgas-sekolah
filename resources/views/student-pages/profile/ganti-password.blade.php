a97338c1545beff047e6d99a66b86be004a047b0
neibreen <azfaalharits25@gmail.com>
2024-11-16 23:19:43 +0700

    chore: add card for profile page

--- resources/views/student-pages/profile/ganti-password.blade.php
+++ resources/views/student-pages/profile/ganti-password.blade.php
@@ -7,35 +7,39 @@
         <x-student.navigation.page-header page-pretitle="overview" :page-title="$title" />
     </x-slot>
 
-    <div class="row g-0">
-        <div class="col-3 d-none d-md-block border-end">
-            <x-student.navigation.sidebar />
-        </div>
-        <div class="col d-flex flex-column">
-            <div class="card-body">
-                <h2 class="mb-4">{{ $title }}</h2>
-                <h3 class="card-title mt-4">Business Profile</h3>
-                <p class="card-subtitle">This contact will be shown to others publicly, so choose it carefully.</p>
-                <div class="row g-3">
-                    <div class="col-md">
-                        <div class="form-label">Business Name</div>
-                        <input type="text" class="form-control" value="Tabler">
-                    </div>
-                    <div class="col-md">
-                        <div class="form-label">Business ID</div>
-                        <input type="text" class="form-control" value="560afc32">
-                    </div>
-                    <div class="col-md">
-                        <div class="form-label">Location</div>
-                        <input type="text" class="form-control" value="Peimei, China">
-                    </div>
+    <div class="card">
+        <div class="card-body">
+            <div class="row g-0">
+                <div class="col-3 d-none d-md-block border-end">
+                    <x-student.navigation.sidebar />
                 </div>
-            </div>
-            <div class="card-footer bg-transparent mt-auto">
-                <div class="btn-list justify-content-end">
-                    <a href="#" class="btn btn-primary">
-                        Ganti password
-                    </a>
+                <div class="col d-flex flex-column">
+                    <div class="card-body">
+                        <h2 class="mb-4">{{ $title }}</h2>
+                        <h3 class="card-title mt-4">Business Profile</h3>
+                        <p class="card-subtitle">This contact will be shown to others publicly, so choose it carefully.</p>
+                        <div class="row g-3">
+                            <div class="col-md">
+                                <div class="form-label">Business Name</div>
+                                <input type="text" class="form-control" value="Tabler">
+                            </div>
+                            <div class="col-md">
+                                <div class="form-label">Business ID</div>
+                                <input type="text" class="form-control" value="560afc32">
+                            </div>
+                            <div class="col-md">
+                                <div class="form-label">Location</div>
+                                <input type="text" class="form-control" value="Peimei, China">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer bg-transparent mt-auto">
+                        <div class="btn-list justify-content-end">
+                            <a href="#" class="btn btn-primary">
+                                Ganti password
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
